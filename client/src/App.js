970955a4ff3405f82f2e48944238b0dc870e8f63
nowaveosu <nowaveosu@gmail.com>
2023-05-03 13:34:41 +0900

    [FE] 라우트+경로설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./page/Home";
+import Login from "./page/Login";
+import Community from "./page/Community";
+import Survey from "./page/Survey";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/commu" element={<Community />}></Route>
+        <Route path="/survey" element={<Survey />}></Route>
       </Routes>
     </BrowserRouter>
   );
