19c1e8586ad360b395cf93b701a4f0d270816e0b
nowaveosu <nowaveosu@gmail.com>
2023-05-03 13:11:24 +0900

    [FE] import react 추가

--- client/src/common/Header.js
+++ client/src/common/Header.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import React from "react";
+import logo from "../../public/logo.png";
 
 export const Header = () => {
-  return;
+  return(
+    <div>hi~i'm header</div>
+  );
 };
 
 export default Header;
