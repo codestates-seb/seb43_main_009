87fd35e27b40dfbf3157026a8230d94523d19c7e
nowaveosu <nowaveosu@gmail.com>
2023-05-03 14:07:34 +0900

    [FE] webpack padding 제거

--- client/src/common/Header.js
+++ client/src/common/Header.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import React from "react";
+import styled from "styled-components";
 import logo from "../../public/logo.png";
 import Home from "../page/Home";
 import Community from "../page/Community"
@@ -7,12 +7,20 @@ import Login from "../page/Login"
 import Survey from "../page/Survey"
 import { Link } from "react-router-dom";
 
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 150px;
+  border : 1px solid black;
+`;
+
 export const Header = () => {
   return(
-    <>
+    <HeaderWrapper>
       <div>hi~i'm header</div>
       <Link to="/login">login</Link>
-    </>
+    </HeaderWrapper>
   );
 };
 
