23947960e2e1bcddd77e6a69edbee1b3030d1b0c
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-05-03 15:53:41 +0900

    redux 초기설정

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from "./App";
-import './index.css';
+import "./index.css";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
