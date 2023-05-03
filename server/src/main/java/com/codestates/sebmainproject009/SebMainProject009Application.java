1ec5eea7c23d339fa330880beb60e439c4d061af
donghee <jjj4435@naver.com>
2023-05-03 12:48:00 +0900

    first commit

---
+++ server/src/main/java/com/codestates/sebmainproject009/SebMainProject009Application.java
@@ -0,0 +1,13 @@
+package com.codestates.sebmainproject009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebMainProject009Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebMainProject009Application.class, args);
+	}
+
+}
