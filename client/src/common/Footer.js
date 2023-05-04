86cb98091ffd24c996d8947e8ebc8fd1f6e0bee5
ggggggggithub <hamtoli5@gmail.com>
2023-05-04 17:36:32 +0900

    [FE] commu main page

--- client/src/common/Footer.js
+++ client/src/common/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import FooterStyle from "../style/FooterStyle";
-import React from "react";
 
 export const Footer = () => {
   return <FooterStyle>hi~i'm footer</FooterStyle>;
--- client/src/page/Commpost.js
+++ client/src/page/Commpost.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 import React from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 const CommpostDesign = styled.div`
   margin: 0;
@@ -36,16 +39,46 @@ const CommpostDesign = styled.div`
   }
 `;
 
-const Commpost = () => {
+const Commpost = (props) => {
+  const { data } = props;
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setpageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 6;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data && data.slice(itemOffset, endOffset));
+    setpageCount(Math.ceil(data && data.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
+
   return (
     <CommpostDesign>
-      <ul className="list">
-        <li className="postid">13</li>
-        <li className="postname">조규현</li>
-        <li className="posttitle">광화문 비타민 핫딜</li>
-        <li className="postview">33</li>
-        <li className="postcreat">2023.05.20</li>
-      </ul>
+      {currentItems &&
+        currentItems.map((el) => (
+          <ul className="list">
+            <li className="postid">{el.commuId}</li>
+            <li className="postname">{el.diplayName}</li>
+            <li className="posttitle">{el.title}</li>
+            <li className="postview">{el.view}</li>
+            <li className="postcreat">{el.createdAt}</li>
+          </ul>
+        ))}
+
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
     </CommpostDesign>
   );
 };
