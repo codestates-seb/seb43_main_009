8c6d5eb29e71330f462bc7baa82f2b22b1338fd4
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-05-08 14:47:19 +0900

    env 수정

--- client/src/page/user/Signup.js
+++ client/src/page/user/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Layout from "../../common/Layout";
-import logo from "../../public/logo.png";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
