0edb4d17f9108142d05ec898845584c6c9bc662d
nowaveosu <nowaveosu@gmail.com>
2023-05-08 21:25:52 +0900

    [FE][Feat] survey 결과 axios 추가

--- client/src/page/survey/Survey.js
+++ client/src/page/survey/Survey.js
@@ -1,38 +1,57 @@
-import React, { useState } from 'react'; // useState import 
+import React, { useEffect, useState } from 'react'; 
 import Step1 from './SurveyStep/Step1';  
 import Step2 from './SurveyStep/Step2';
 import Step3 from './SurveyStep/Step3';
-import Step4 from './SurveyStep/Step4'; //스텝별로 만들어둔 컴포넌트 import
+import Step4 from './SurveyStep/Step4'; 
+import { useNavigate } from "react-router-dom";
+import { getCookie } from "../../utils/cookies";
+import axios from 'axios';
 import Layout from "../../common/Layout";
 
 const Survey = () => {
-    const[step, setStep] = useState(1) //페이지 전환을 위한 useState
+    const[step, setStep] = useState(1) 
+    const navigate = useNavigate();
     const[form, setForm] = useState(
         {
             disease : '',
             allergy : '',
         }
-    ) //사용자로부터 입력받을 값들 초기화(default값 설정)
+    ) 
     
     const {disease, allergy} = form
-    //JSX내에서 간단하게 사용하기 위해 비구조할당
 
-    const changeInput = (e)=>{ 		//input란에 이벤트(e)가 생길경우 
-        const {value, name} = e.target //그 value값을 해당 name(속성값)에 받아서
+    const submitForm = async() => {
+        try{
+            const response = await axios.post('http://ec2-3-34-134-67.ap-northeast-2.compute.amazonaws.com:8080/surveys',form);
+            console.log(response.data);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    const changeInput = (e)=>{ 		
+        const {value, name} = e.target 
         setForm({
-            ...form, 			//기존 데이터는 그대로 유지하는 스프래드연산spread
-            [name] : value 		// 입력 받은 값으로 저장해라!
+            ...form, 			
+            [name] : value 		
         })
     }
 
     const nextSteps =()=>{
-        setStep(step+1)			//다음 페이지로
+        setStep(step+1)			
     } 
     const prevSteps =()=>{
-        setStep(step-1)			//이전 페이지로
+        setStep(step-1)			
     } 
 
     console.log(form);
+
+    // 로그인 안했으면 회원가입 페이지로 보냄
+    // useEffect(() => {
+    //     if (getCookie("token") === "null") {
+    //       navigate("/signup");
+    //     }
+    //   });
 
     return (
       <Layout>
@@ -65,10 +84,10 @@ const Survey = () => {
                 allergy = {allergy}
                 changeInput={changeInput}
                 prevSteps={prevSteps}
-                nextSteps={nextSteps}
+                submitForm={submitForm}
+                form = {form}
                 />
             }
-            
         </div>
       </Layout>
 
--- client/src/page/survey/SurveyStep/Step4.js
+++ client/src/page/survey/SurveyStep/Step4.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const Step4 = ({ disease,  allergy, nextSteps, prevSteps}) => {
+const Step4 = ({ disease,  allergy, nextSteps, prevSteps, form, submitForm}) => {
+    console.log(form);
     return (
-        <>
+    <>
         <p>
-            <label>병  </label> :
+            <label>병</label> :
             <span  name="disease">{disease}</span>
         </p>
 
         <p>
-            <label>직업  </label> :
+            <label>직업</label> :
             <span  name="allergy">{allergy}</span>
         </p>
 
         <p>
         <button onClick={prevSteps}>이전</button>
-        <button onClick={nextSteps}>다음</button>
+        <button onClick={submitForm}>제출</button>
         </p>
         
     </>
