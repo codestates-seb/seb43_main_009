102149130b0bc00498654afd39d955d94e3d6f8c
Sniij <dokeycho@gmail.com>
2023-05-08 11:41:02 +0900

    [BE][Test] Security Setting 테스트 : 로그인 권한 삭제

--- server/src/main/java/com/codestates/sebmainproject009/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/sebmainproject009/config/SecurityConfiguration.java
@@ -1,78 +1,36 @@
 package com.codestates.sebmainproject009.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.Collections;
-
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {//인증 방식과 웹페이지 접근 권한 설정
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http
                 .csrf().disable()
+                .cors().and()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // for CORS preflight request
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/auth/login-form")
-                .loginProcessingUrl("/auth/process-login")
-                .failureUrl("/auth/login-form?error")
-                .defaultSuccessUrl("/")
-                .and()
-                .logout()
-                .logoutUrl("/auth/logout")
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+                .anyRequest().permitAll();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("https://dowajoyak.shop"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
+        configuration.setAllowedOrigins(Arrays.asList("https://dowajoyak.shop"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
         return source;
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
-    @Bean
-    public UserDetailsManager userDetailsManager(){//인터페이스
-        UserDetails userDetails =
-                User.withDefaultPasswordEncoder()//패스워드 암호화
-                        .username("kevin@gmail.com")//고유 사용자를 식별할 수 있는 아이디
-                        .password("1111")//1111를 암호화
-                        .roles("USER")//역할 지정
-                        .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
     }
 }
