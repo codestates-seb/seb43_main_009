361c182adc89cee8aaa939ef2d9fe67ea6974af4
ggggggggithub <hamtoli5@gmail.com>
2023-05-09 18:49:16 +0900

    [FE][Design]Surveycss

--- client/src/page/survey/SurveyStep/Step2.js
+++ client/src/page/survey/SurveyStep/Step2.js
@@ -1,34 +1,130 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import dryskin from "../../../../public/dryskin.png";
+import fatigue from "../../../../public/fatigue.png";
+import { GiStomach } from "react-icons/gi";
+import { ImEye } from "react-icons/im";
+import { BsShieldPlus } from "react-icons/bs";
+
+const StyledGistomach = styled(GiStomach)`
+  margin-left: 37%;
+  margin-right: 10px;
+`;
+
+const StyledImEye = styled(ImEye)`
+  margin-left: 37%;
+  margin-right: 10px;
+`;
+
+const StyledBsShieldPlus = styled(BsShieldPlus)`
+  margin-left: 37%;
+  margin-right: 10px;
+`;
+
+const Step2Design = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f1f2f3;
+
+  img {
+    width: 35px;
+    height: 35px;
+    margin-left: 37%;
+    margin-right: 10px;
+  }
+
+  button {
+    width: 30vw;
+    height: 60px;
+    border: 1px solid black;
+    border-radius: 20px;
+    background-color: #f9e6e6;
+    font-size: 35px;
+    display: flex;
+    align-items: center;
+  }
+  .selected {
+    background-color: #f05858;
+  }
+  .choice {
+    font-size: 35px;
+  }
+  .next,
+  .nothing {
+    margin: 0 auto;
+  }
+`;
 
 const Step2 = ({ disease, changeInput, nextSteps }) => {
+  const menu = {
+    stomach: "소화",
+    skin: "피부",
+    eye: "눈",
+    sheild: "면역",
+    fatiuge: "피로",
+    nothing: "없음",
+  };
+
   const handleDiseaseClick = (e) => {
     changeInput({ target: { name: "disease", value: e.target.value } });
   };
 
   return (
-    <>
-      <p>
-        <label>불편한곳 </label> :
-        <button value="소화" onClick={handleDiseaseClick}>
-          소화, 장
-        </button>
-        <button value="피부" onClick={handleDiseaseClick}>
-          피부
-        </button>
-        <button value="눈" onClick={handleDiseaseClick}>
-          눈
-        </button>
-        <button value="면역" onClick={handleDiseaseClick}>
-          면역
-        </button>
-        <button value="피로" onClick={handleDiseaseClick}>
-          피로
-        </button>
-      </p>
-      <p>
-        <button onClick={nextSteps}>다음</button>
-      </p>
-    </>
+    <Step2Design>
+      <div className="choice">불편하시거나 걱정되시는 곳을 선택해 주세요! </div>
+      <button
+        className={disease === menu.stomach ? "selected" : ""}
+        value="소화"
+        onClick={handleDiseaseClick}
+      >
+        <StyledGistomach /> 소화, 장
+      </button>
+      <button
+        className={disease === menu.skin ? "selected" : ""}
+        value="피부"
+        onClick={handleDiseaseClick}
+      >
+        <img src={dryskin} alt="dryskin" />
+        피부
+      </button>
+      <button
+        className={disease === menu.eye ? "selected" : ""}
+        value="눈"
+        onClick={handleDiseaseClick}
+      >
+        <StyledImEye /> 눈
+      </button>
+      <button
+        className={disease === menu.sheild ? "selected" : ""}
+        value="면역"
+        onClick={handleDiseaseClick}
+      >
+        <StyledBsShieldPlus />
+        면역
+      </button>
+      <button
+        className={disease === menu.fatiuge ? "selected" : ""}
+        value="피로"
+        onClick={handleDiseaseClick}
+      >
+        <img src={fatigue} alt="fatiuge" /> 피로
+      </button>
+      <button
+        className={disease === menu.nothing ? "selected" : ""}
+        value="없음"
+        onClick={handleDiseaseClick}
+      >
+        <span className="nothing">없음</span>
+      </button>
+      <button onClick={nextSteps}>
+        <span className="next">다음 ▶︎</span>
+      </button>
+    </Step2Design>
   );
 };
 
--- client/src/page/survey/SurveyStep/Step5.js
+++ client/src/page/survey/SurveyStep/Step5.js
@@ -1,28 +1,124 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import high from "../../../../public/high.jpg";
+import logo from "../../../../public/logo.png";
 
-const Step5 = ({form}) => {
-    // 소화 , 피부, 눈 , 면역, 피로
-    let pill = ["유산균","콜라겐","비타민A","진세노사이드","비타민B"];
-    let resultPill = "";
-    if(form.disease === "소화"){
-        resultPill = pill[0];
-    }else if (form.disease === "피부"){
-        resultPill = pill[1];
-    }else if (form.disease === "눈"){
-        resultPill = pill[2];
-    }else if (form.disease === "면역"){
-        resultPill = pill[3];
-    }else if (form.disease === "피로"){
-        resultPill = pill[4];
-    }
-    return (
-    <>
-        <div>당신의 병은 {form.disease} 입니다.</div>
-        <div>당신을 위한 약은 {resultPill} 입니다.</div>
+const SurveyResultDesign = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f1f2f3;
+  font-size: 22px;
 
-        
-    </>
-    );
+  img {
+    width: 150px;
+    height: 50px;
+  }
+  .whoresult {
+    border-bottom: 1px solid black;
+    margin-top: 10vh;
+  }
+
+  div span {
+    margin-left: 5vw;
+  }
+  .explain {
+    margin-top: 7vh;
+    margin-left: 5vw;
+  }
+  .explaindetail {
+    width: 80vw;
+    margin-top: 5vh;
+  }
+  .helppill {
+    margin-top: 5vh;
+    margin-left: 1vw;
+  }
+
+  .recommend {
+    background-color: #f5d6d6;
+    margin-top: 5vh;
+    margin-left: 5vw;
+    width: 80vw;
+    height: 50vh;
+    flex-wrap: wrap;
+  }
+  .efficacy {
+    margin-top: 5vh;
+  }
+
+  .buynow {
+    margin-left: 2vw;
+    margin-top: 20px;
+  }
+  .retry {
+    background-color: #f05858;
+    border: none;
+    margin-left: 5vw;
+    width: 160px;
+    height: 70px;
+    font-size: 20px;
+    color: white;
+  }
+  .high {
+  }
+`;
+
+const Step5 = (/*{form}*/) => {
+  // 소화 , 피부, 눈 , 면역, 피로
+
+  let form = {
+    disease: "소화",
+  };
+
+  let pill = ["유산균", "콜라겐", "비타민A", "진세노사이드", "비타민B"];
+  let resultPill = "";
+  if (form.disease === "소화") {
+    resultPill = pill[0];
+  } else if (form.disease === "피부") {
+    resultPill = pill[1];
+  } else if (form.disease === "눈") {
+    resultPill = pill[2];
+  } else if (form.disease === "면역") {
+    resultPill = pill[3];
+  } else if (form.disease === "피로") {
+    resultPill = pill[4];
+  }
+  return (
+    <SurveyResultDesign>
+      <div className="whoresult">
+        <span>name님의 필요영양성분</span>
+      </div>
+
+      <div className="explain">
+        <div>옥타코사놀은 면역력 증진에 도움을 줄 수 있습니다.</div>
+        <div className="explaindetail">
+          옥타코사놀은 생리활성물질로 현미, 사탕수수, 사과/포도의 껍질에
+          함유되어 있으며 철새의 장거리 이동하는 에너지의 근원이라는 연구도
+          있습니다. 옥타코사놀을 매일 7mg이상 섭취하는 경우에 식약처에서는
+          지구력 증진에 도움을 줄 수 있다고 인정하고 있습니다.
+        </div>
+      </div>
+      <div className="recommend">
+        <div className="helppill">
+          <img src={logo} alt="logo" />
+          에서 추천하는 옥타코사놀 영양제
+        </div>
+        <ul className="efficacy">
+          <li>국내산 6년근 홍삼 사용</li>
+          <li>면역력 증진을 위한 옥타코사놀</li>
+          <li>천연 비타민 E의 황산화효과</li>
+        </ul>
+        <a className="buynow" href="https://www.naver.com">
+          구매하러 가기
+        </a>
+        <button className="retry">맞춤검사 다시하기</button>
+        <img className="high" src={high} alt="high" />
+      </div>
+    </SurveyResultDesign>
+  );
 };
 
 export default Step5;
