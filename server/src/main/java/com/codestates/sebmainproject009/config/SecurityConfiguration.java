4f333f2d5eaafd3337c0081eaebf6ce7bf71da82
Sniij <120099321+Sniij@users.noreply.github.com>
2023-05-09 19:00:28 +0900

    [BE][Refactor] SecurityConfiguration.java 수정
    : debug 추가

--- server/src/main/java/com/codestates/sebmainproject009/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/sebmainproject009/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
