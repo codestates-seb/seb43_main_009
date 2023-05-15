6bd0f86785304e6bbbac18caefc675482f045391
nowaveosu <nowaveosu@gmail.com>
2023-05-15 11:16:23 +0900

    [FE][Fix] 커뮤니티 작성자변경 완료

--- client/src/utils/UserInfo.js
+++ client/src/utils/UserInfo.js
@@ -1,10 +1,12 @@
 export const getUserInfo = () => {
   const token = localStorage.getItem('accessToken');
+  console.log(token);
   if (!token) {
     return null;
   }
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const payload = JSON.parse(atob(base64));
+  console.log(payload);
   return payload;
 };
