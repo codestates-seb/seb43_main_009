0eb82c4dae1d4e0d4342092448a90625592bd108
donghee <jjj4435@naver.com>
2023-05-16 15:28:27 +0900

    [BE][Feat] Survey 구현

---
+++ server/src/main/java/com/codestates/sebmainproject009/survey/Dto/SurveyDto.java
@@ -0,0 +1,12 @@
+package com.codestates.sebmainproject009.survey.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SurveyDto {
+      private long userId;
+      private String disease;
+      private String allergy;
+}
