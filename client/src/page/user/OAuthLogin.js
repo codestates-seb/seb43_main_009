0f37b957105fa592ec5f04af300e7f4fbb4a659f
nowaveosu <nowaveosu@gmail.com>
2023-05-16 11:01:18 +0900

    [FE][Fix]로그인후 이전페이지 보내기

--- client/src/page/user/OAuthLogin.js
+++ client/src/page/user/OAuthLogin.js
@@ -13,7 +13,7 @@ const OAuthLogin = ({ onLogin }) => {
       localStorage.setItem('accessToken', accessToken);
       localStorage.setItem('refreshToken', refreshToken);
       onLogin();
-      navigate('/');
+      navigate(-1);
     }
   }, [location, onLogin]);
 
