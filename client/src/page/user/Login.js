2f09fa77c6e32e9459923fdbe91d2ac8c23d99c4
nowaveosu <nowaveosu@gmail.com>
2023-05-26 16:39:34 +0900

    [FE][Fix] 이메일, 패스워드 기본값 공란으로 변경

--- client/src/page/user/Login.js
+++ client/src/page/user/Login.js
@@ -33,8 +33,8 @@ import {
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [email, setEmail] = useState('hello@gmail.com');
-  const [password, setPassword] = useState('1234');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   window.scrollTo(0, 0);
 
   const handleSubmit = async (e) => {
